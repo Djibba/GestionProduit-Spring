6cb991096795e0370adbf9343ec3b7d37e9695c2
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-07-10 22:43:09 +0000

    add CategorieRESTController

---
+++ src/main/java/com/gestionproduits/restControllers/CategorieRESTController.java
@@ -0,0 +1,26 @@
+package com.gestionproduits.restControllers;
+
+import com.gestionproduits.entities.Categorie;
+import com.gestionproduits.repos.CategorieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cat")
+@CrossOrigin
+public class CategorieRESTController {
+    @Autowired
+    CategorieRepository categorieRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categorie> getAllCategories(){
+        return categorieRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Categorie getCategorieById(@PathVariable("id") Long id){
+        return categorieRepository.findById(id).get();
+    }
+}
